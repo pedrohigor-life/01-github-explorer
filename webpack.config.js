c040325e8cf069097d97265e7a77d44cd79961e7
Pedro Sousa <pedrohigor.dev@gmail.com>
2023-05-03 19:55:03 -0300

    Configuring webpack for read statics files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: "development",
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -11,6 +15,12 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
 
   module: {
     rules: [
